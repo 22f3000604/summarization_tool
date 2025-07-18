bb5f7b5be3803946902e61588ffede1aeb045fbb
AKSHIT <132344370+22f3000604@users.noreply.github.com>
2025-07-18 19:32:47 +0000

    Correct requirements.txt file name and update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ model_selection = st.selectbox("Select a model",["facebook/bart-large-cnn", "goo
 if st.button("Summarizer"):
     if text:
         summarizer = pipeline("summarization", model=model_selection)
-        summary = summarizer(text, max_length=130, min_length=30, do_sample=False)
+        summary = summarizer(text, max_length=200, min_length=120, do_sample=False)
         st.write("Summary:")
         st.write(summary[0]['summary_text'])
     else:
